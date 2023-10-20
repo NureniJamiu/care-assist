44c426cbb44d8892406d02da96144e11b138e23d
NureniJamiu <nurenijamiu951@gmail.com>
2023-10-20 10:53:39 +0100

    fix: hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,18 +13,24 @@ const Hero = () => {
       <div className="flex flex-col lg:flex-row items-center gap-8 text-center lg:text-left">
         <div className="flex-1" data-aos="fade-up" data-aos-duration="1000">
           <h1
-            className={`${raleway.className} text-[#1E1F4B] text-5xl lg:text-6xl font-bold md:max-w-[600px] md:mx-auto lg:w-full`}
+            className={`${raleway.className} text-[#1E1F4B] text-5xl lg:text-6xl font-bold md:mx-auto lg:w-full`}
           >
             A Journey of Care and Connection
           </h1>
-          <p className="py-8 text-zinc-700 md:w-[400px] md:mx-auto lg:mx-0 text-xl">
+          <p className="py-8 text-zinc-700 md:w-[400px] lg:w-[700px] md:mx-auto lg:mx-0 text-xl">
             Help find solutions with intuitive and in accordance with the client
             business goals. We provide high-quality services.
           </p>
           <Button title="Get Started Today" className="px-10 py-4 text-base" />
         </div>
         <div className="flex-1" data-aos="fade-up" data-aos-duration="1000">
-          <Image src={sectionImage} alt="Hero" width={530} height={530} />
+          <Image
+            src={sectionImage}
+            alt="Hero"
+            width={530}
+            height={530}
+            className="float-right"
+          />
         </div>
       </div>
     </section>
